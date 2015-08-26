c81edd5180bf556849c8a78e86d5d4e7f51a7f38
ee7klt <kltah1@gmail.com>
2015-08-26 15:25:01 +0800

    beautify. add checkbox and delete button

--- ReactStarter/src/list-item.jsx
+++ ReactStarter/src/list-item.jsx
@@ -2,12 +2,30 @@ var React = require('react');
 
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {text: this.props.item.text}
+  },
+  handleButtonClick: function() {
+    this.setState(
+      {text:''}
+    )
+  },
 
   render: function() {
     console.log(this.props.item)
-    return <li>
-      {this.props.item.text} {this.props.item['.key']}  {''+ this.props.item.done}
-
-    </li>
+    return <div className = "input-group">
+        <span className = "input-group-addon">
+          <input type="checkbox" ></input>
+          <input type="text" className = "form-control" value={this.state.text}></input>
+          <span className = "input-group-btn">
+            <button
+              className="btn btn-default"
+              onClick = {this.handleButtonClick}
+              >
+              Delete
+            </button>
+          </span>
+        </span>
+    </div>
   }
 })
