54597dab325af95c079ca5956ed8486f26fa386d
ee7klt <kltah1@gmail.com>
2015-09-16 12:04:46 +0800

    app kicks it all off, routes defines routing structure, main is root

--- imgur-client/src/app.jsx
+++ imgur-client/src/app.jsx
@@ -2,32 +2,8 @@ var React = require('react');
 var Routes = require('./routes');
 
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-    {this.props.children}
-    </h1>
-  }
-});
-
-//dummy, text components
-
-var Child1 = React.createClass({
-  render: function() {
-    return <h1>
-      I'm child1
-      {this.props.children}
-    </h1>
-
-  }
-});
-
-var Child2 = React.createClass({
-  render: function() {
-    return <h1> I'm child2</h1>
-  }
-});
 
 
-//var element = React.createElement(Hello, {});
+
+
 React.render(Routes, document.querySelector('.container'));
