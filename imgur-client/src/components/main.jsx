0380a24090476fc9951bab1e322ac8af3630a77d
ee7klt <kltah1@gmail.com>
2015-09-16 12:46:59 +0800

    use renderCount to prove that page is re-rendering

--- imgur-client/src/components/main.jsx
+++ imgur-client/src/components/main.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
 var Header = require('./header.jsx');
+var renderCount = 0;
+
 
 module.exports = React.createClass({
   render: function() {
+    console.log(renderCount++);
     return <div>
       <Header />
       {this.props.children}
