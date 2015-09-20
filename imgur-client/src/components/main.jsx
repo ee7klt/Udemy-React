ae7da66bbc3c5126f7d7015e5a8f227e18e156aa
ee7klt <kltah1@gmail.com>
2015-09-20 13:43:10 +0800

    47. create topics-list component and show it at root only

--- imgur-client/src/components/main.jsx
+++ imgur-client/src/components/main.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 var Header = require('./header.jsx');
-
+var TopicList = require('./topics-list');
 
 
 module.exports = React.createClass({
   render: function() {
     return <div>
       <Header />
-      {this.props.children}
+      {this.content()}
     </div>
+  },
+
+  content: function() {
+    if (this.props.children) {
+      return this.props.children
+    }  else {
+      return <TopicList />
+    }
   }
 })
