23e257d19d16d8d98304b03c48def534a4e0a8dd
ee7klt <kltah1@gmail.com>
2015-10-04 23:01:19 +0800

    refactor images in to image-preview. fetch the small thumbnail preview

--- imgur-client/src/components/topic.jsx
+++ imgur-client/src/components/topic.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ImageStore = require('../stores/image-store')
 var Reflux = require('reflux')
 var Actions = require('../actions')
+var ImagePreview = require('./image-preview.jsx')
 
 module.exports = React.createClass({
 
@@ -30,9 +31,9 @@ module.exports = React.createClass({
 
   renderImages: function() {
     console.log(this.state.images)
-    return this.state.images.slice(0,4).map(function(image) {
-      console.log(image.id)
-      return <li> <img src={'http://i.imgur.com/'+image.id+'.png'}/></li>
+    return this.state.images.slice(0,10).map(function(image) {
+      //console.log(image.id)
+      return <ImagePreview key={image.id} {...image}/>
     })
   },
   render: function() {
