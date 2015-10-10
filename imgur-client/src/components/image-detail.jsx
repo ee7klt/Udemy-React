1c7c175a41f3eed14efc9a77f062d666cb2ccf9c
ee7klt <kltah1@gmail.com>
2015-10-10 17:53:28 +0800

    yes it isgit add .

--- imgur-client/src/components/image-detail.jsx
+++ imgur-client/src/components/image-detail.jsx
@@ -2,27 +2,13 @@ var React=require('react');
 
 module.exports = React.createClass({
   render: function() {
-    return <div> this is image detail</div>
-  },
-  image: function() {
-      var link = 'http://i.imgur.com/' + this.props.id + 'm.jpg'
-      return <img src = {link}/>
-  },
-
-  video: function() {
-
-    var link = this.props.webm
-    return <div>
-      <video
-        autoPlay
-        loop
-        autoBuffer
-        preload = 'auto'
-        webkit-playsinline
-        >
-        <source src={link} type='video/mp4'></source>
-
-      </video>
+    return <div> this is image {this.props.params.id}
+    <br />
+      {this.image()}
     </div>
   },
+  image: function() {
+      var link = 'http://i.imgur.com/' + this.props.params.id + 'm.jpg'
+      return <img src = {link}/>
+  }
 })
